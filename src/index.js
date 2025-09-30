3346f3db0e5ce01c117ab6183ceeff6df02c959d
osioefren <efrenosio.iflibaojt@gmail.com>
2025-10-01 00:29:02 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { registerBlockType } from '@wordpress/blocks';
+import { useBlockProps } from '@wordpress/block-editor';
+
+registerBlockType('proevent/event-grid', {
+    title: 'Event Grid',
+    icon: 'calendar',
+    category: 'widgets',
+    edit() {
+        return <div {...useBlockProps()}>📅 Event Grid (Preview)</div>;
+    },
+    save() {
+        return null; // gagamitin yung render_callback sa PHP
+    },
+});
