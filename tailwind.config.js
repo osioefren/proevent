3f55e54252dddc5ea690a16cbfa8318aa1001a17
osioefren <efrenosio.iflibaojt@gmail.com>
2025-09-30 23:12:50 +0800

    save

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.php",
+    "./inc/**/*.php",
+    "./parts/**/*.php",
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
