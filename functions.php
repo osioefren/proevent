d91df3d6d5bb8170f228e696ec9ae5a5b8b92e05
osioefren <efrenosio.iflibaojt@gmail.com>
2025-09-30 23:25:29 +0800

    update

--- functions.php
+++ functions.php
@@ -1,19 +1,23 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) exit; 
+if ( ! defined( 'ABSPATH' ) ) exit;  
 
-
+// Enqueue Tailwind build output
 function proevent_enqueue_scripts() {
-    wp_enqueue_style( 'proevent-style', get_stylesheet_uri() );
+    wp_enqueue_style(
+        'proevent-style',
+        get_template_directory_uri() . '/dist/style.css',
+        [],
+        filemtime( get_template_directory() . '/dist/style.css' ) // cache-busting
+    );
 }
 add_action( 'wp_enqueue_scripts', 'proevent_enqueue_scripts' );
-
 
 function proevent_theme_setup() {
     add_theme_support( 'title-tag' );
     add_theme_support( 'post-thumbnails' );
 }
-add_action( 'after_setup_theme', 'proevent_theme_setup' );
 
+add_action( 'after_setup_theme', 'proevent_theme_setup' );
 
 function proevent_register_features() {
     // menus
